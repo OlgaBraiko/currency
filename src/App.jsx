0ec7be72539a41019748e2f7a1eca0d17fa69f51
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-17 20:57:21 -0700

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
+import Block from "./components/Block";
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
+      <Block value={0} currency="USD" />
+    </div>
+  );
+}
 
 export default App;
